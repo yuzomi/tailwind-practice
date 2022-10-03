0121dff7ed121d624a16468ee558b23610b30eba
yuzomi <im.the.best@kakao.com>
2022-10-04 01:34:36 +0900

    폰트 수정

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["IBM Plex Sans KR", "sans-serif"],
+        hi: ['"Gamja Flower"', "cursive"],
+      },
+    },
+    plugins: [],
+  },
+};
